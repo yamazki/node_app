3340743a4a4670404707b5963acae16de9f56d85
yamazaki <ma17123@shibaura-it.ac.jp>
2018-05-14 20:25:33 +0900

    vue.js test

--- node/app.js
+++ node/app.js
@@ -7,7 +7,7 @@ app.get("/", function(req, res){
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.listen(3000, function () {
+http.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
 
